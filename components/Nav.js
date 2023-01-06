f30e50225fab4c67c8d42929a7d725519d12c6eb
abel <abelw34@gmail.com>
2023-01-06 18:32:14 +0300

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,25 @@
+import { AiOutlineCode } from "react-icons/ai";
+
+function Nav() {
+  return (
+    <div>
+      <div>
+        <div>
+          <AiOutlineCode />
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Docs</li>
+              <li>Github</li>
+              <li>Buy me a coffee</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
